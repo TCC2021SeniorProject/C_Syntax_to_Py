06182656a50e81435d4e2120418ec5ab2f9afadc
MarcoBackman <roni2006@hanmail.net>
2022-04-20 08:54:20 -0400

    Single operator implemented

--- src/syntax_converter.py
+++ src/syntax_converter.py
@@ -10,18 +10,17 @@
 """
 
 #Left - C syntax, Right - Python syntax
-conditional_single_operator = { "&" : 1,
-                                "|" : 1,
-                                ">" : 3,
-                                "<" : 3,
-                                "=" : 3,
-                                ">" : 3,
-                                "<" : 3,
-                                "!" : 4,
-                                ":" : 5,
-                                "+" : 5,
-                                "-" : 5,
-                                "*" : 6,}
+conditional_single_operator = { "&" : "&",
+                                "|" : "|",
+                                ">" : ">",
+                                "<" : "<",
+                                "=" : "=",
+                                ">" : ">",
+                                "<" : "<",
+                                ":" : ":",
+                                "+" : "+",
+                                "-" : "-",
+                                "*" : "*",}
 
 conditional_operator = {"&&" : "and",
                         "||" : "or",
@@ -32,7 +31,6 @@ conditional_operator = {"&&" : "and",
                         "<" : "<",
                         "!=" : "!=",
                         "=" : "=",
-                        "<=" : "=",
                         ":=" : "=",
                         "//=" : "//=",
                         "//=" : "//=",
@@ -155,10 +153,8 @@ class SyntaxTree:
                 return
             #On one operator character match
             elif char in conditional_single_operator:
+                #Set for two letter operator first
                 operator = line[index] + line[index + 1]
-                #If not two letter operator, then one letter operator.
-                if not(operator in conditional_operator):
-                    operator = line[index] 
                 if operator in conditional_operator:
                     if type(walk) is CharNode:
                         if walk.next == None:
@@ -215,8 +211,64 @@ class SyntaxTree:
                                 walk.set_parent(new_parent)
                                 self.translate(line[(index + 2):].strip(), new_parent) #Update parent
                                 return
+                elif operator not in expression: #Single charater operator
+                    operator = line[index]
+                    if type(walk) is CharNode:
+                        if walk.next == None:
+                            new_node = Node(conditional_single_operator[operator])
+                            walk.set_next(new_node)
+                            new_node.set_left(remainder.strip())
+                            new_node.set_parent(walk)
+                            self.translate(line[(index + 1):].strip(), new_node)
+                            return
+                        else:
+                            if walk.parent == self.root: #Always node
+                                walk.parent.set_operator(conditional_single_operator[operator])
+                                self.translate(line[(index + 1):].strip(), walk.parent)
+                                return
 
-            if char in expression: #Detects '(', '!'
+                            new_node = Node(conditional_single_operator[operator])
+                            if type(walk) is CharNode:
+                                walk.set_next(new_node)
+                            else:
+                                walk.set_left(new_node)
+                            new_node.set_parent(walk)
+                            new_node.set_left(remainder.strip())
+                            self.translate(line[(index + 1):].strip(), new_node)
+                            return
+                    else:
+                        if walk.left == None: #Normal case
+                            walk.set_left(remainder.strip())
+                            walk.set_operator(conditional_single_operator[operator])
+                            self.translate(line[(index + 1):].strip(), walk) #Update parent
+                            return
+                        elif walk.right == None: #Right is empty
+                            #Take right
+                            new_child = Node(conditional_single_operator[operator])
+                            new_child.set_left(remainder.strip())
+                            new_child.set_parent(walk)
+                            walk.set_right(new_child)
+                            self.translate(line[(index + 1):].strip(), new_child) #Update parent
+                            return
+                        else: #Every children field is full
+                            new_parent = Node(conditional_single_operator[operator])
+                            if walk == self.root:
+                                new_parent.set_left(walk)
+                                walk.set_parent(new_parent)
+                                self.root = new_parent
+                                self.translate(line[(index + 1):].strip(), new_parent) #Update parent
+                                return
+                            else:
+                                if type(walk.parent) is CharNode:
+                                    walk.parent.set_next(new_parent)
+                                else:
+                                    walk.parent.set_left(new_parent)
+                                new_parent.set_parent(walk.parent)
+                                new_parent.set_left(walk)
+                                walk.set_parent(new_parent)
+                                self.translate(line[(index + 1):].strip(), new_parent) #Update parent
+                                return
+            if line[index] in expression: #Detects '(', '!'
                 if type(walk) is CharNode:
                     char_node = CharNode(expression[char], walk)
                     walk.set_next(char_node)
@@ -252,57 +304,57 @@ class SyntaxTree:
             return script + ")" if walk.value == "(" else script
         return script
 
-string1 = "a>= 2&&!b !=true" #-> Pass: ((a >= 2) and not b) != True
+string1 = "a>= 2    &&!b !=true" #-> Pass: ((a >= 2) and not b) != True
 
 syntax = SyntaxTree(string1)
-print(syntax.get_py_script(syntax.root, ""))
+result = syntax.get_py_script(syntax.root, "")
 print("-----------------------------")
 
-string2 = "a>=2&&!(b!=false)" #-> Fail: ((a >= 2) and not b) != False
+string2 = "a>=2&&!(b!=false)" #-> Pass: ((a >= 2) and not b) != False
 
 syntax = SyntaxTree(string2)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
-string3 = "(a>=2)&&!(b!=false)" #-> Fail : ((a >= 2) and not b) != False
+string3 = "(a>=2)&&!(b!=false)" #-> Pass : ((a >= 2) and not b) != False
 
 syntax = SyntaxTree(string3)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
 
-string4 = "((a>=2))&&!(b!=4)" #-> Fail - ((a >= 2) and not b) != 4
+string4 = "((a>=2))&&!(b!=4)" #-> Pass: ((a >= 2) and not b) != 4
 
 syntax = SyntaxTree(string4)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
 
-string5 = "(e&&(a>=2))&&!(b!=4)" #-> Fail - (((e and a) >= 2) and not b) != 4
+string5 = "(e&&(a>=2))&&!(b!=4)" #-> Pass: (((e and a) >= 2) and not b) != 4
 
 syntax = SyntaxTree(string5)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
-string6 = "(!a)&&b--" #-> Pass - not a and b -= 1
+string6 = "(!a)&&b--" #-> Pass: not a and b -= 1
 
 syntax = SyntaxTree(string6)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
-string7 = "++b&&(!a)" #-> Pass - b += 1 and not a
+string7 = "++b&&(!a)" #-> Pass: b += 1 and not a
 
 syntax = SyntaxTree(string7)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
-string8 = "a := b++" #-> Pass - a = b += 1
+string8 = "a := b++" #-> Pass: a = b += 1
 
 syntax = SyntaxTree(string8)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
 
-string9 = "var = 0" #-> Pass - a = b += 1
+string9 = "var = 0" #-> Pass: a = b += 1
 
 syntax = SyntaxTree(string9)
 print(syntax.get_py_script(syntax.root, ""))
@@ -317,5 +369,23 @@ print("-----------------------------")
 string11 = "mode = mode   +   1"
 
 syntax = SyntaxTree(string11)
+print(syntax.get_py_script(syntax.root, ""))
+print("-----------------------------")
+
+string12 = "mode <= 1"
+
+syntax = SyntaxTree(string12)
+print(syntax.get_py_script(syntax.root, ""))
+print("-----------------------------")
+
+string13 = "mode >= 1"
+
+syntax = SyntaxTree(string13)
+print(syntax.get_py_script(syntax.root, ""))
+print("-----------------------------")
+
+string14 = "mode += 1"
+
+syntax = SyntaxTree(string14)
 print(syntax.get_py_script(syntax.root, ""))
 print("-----------------------------")
